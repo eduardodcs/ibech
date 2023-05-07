52357a68514a9e5642134b61da9f7d5061af5ba1
Eduardo Silva <eddcesilva@gmail.com>
2023-05-07 15:33:30 -0300

    Adicionando cadastros DBService

--- src/main/java/br/com/mack/ibech/services/DBService.java
+++ src/main/java/br/com/mack/ibech/services/DBService.java
@@ -27,10 +27,28 @@ public class DBService {
 	public void instaciaDB() {
 		
 		Cliente cli1 = new Cliente(null, "admin", "14868316052", "admin@gmail.com", encoder.encode("admin"));
-		clienteRepository.saveAll(Arrays.asList(cli1));
+		Cliente cli2 = new Cliente(null, "Gilvan Souza", "63570355055", "gilvan@gmail.com", encoder.encode("admin1"));
+		Cliente cli3 = new Cliente(null, "Edlaine Cardoso", "00600819027", "edlaine@gmail.com", encoder.encode("admin2"));
+		Cliente cli4 = new Cliente(null, "José Joaquim", "54626933033", "jose@gmail.com", encoder.encode("admin3"));
+		Cliente cli5 = new Cliente(null, "Avelino Smith", "12167730098", "avelino@gmail.com", encoder.encode("admin4"));
+		Cliente cli6 = new Cliente(null, "Vania Costa", "16390698011", "vania@gmail.com", encoder.encode("admin5"));
+		Cliente cli7 = new Cliente(null, "Odair José", "31596889098", "odair@gmail.com", encoder.encode("admin6"));
+		Cliente cli8 = new Cliente(null, "Bruno Gustavo", "43631310048", "bruno@gmail.com", encoder.encode("admin7"));
+		Cliente cli9 = new Cliente(null, "Adélia Silva", "19425656070", "adelia@gmail.com", encoder.encode("admin8"));
+		Cliente cli10 = new Cliente(null, "Katia Gonzaga", "76212064032", "katia@gmail.com", encoder.encode("admin9"));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6, cli7, cli8, cli9, cli10));
 		
-		Fornecedor forn1 = new Fornecedor(null, "Fornecedor 01", "66162132000128", "fornecedor01@gmail.com.br");
-		fornecedorRepository.saveAll(Arrays.asList(forn1));
+		Fornecedor forn1 = new Fornecedor(null, "Paulista", "66162132000128", "fornecedor01@gmail.com.br");
+		Fornecedor forn2 = new Fornecedor(null, "Saraiva", "29497605000111", "fornecedor02@gmail.com.br");
+		Fornecedor forn3 = new Fornecedor(null, "Abril", "97521662000149", "fornecedor03@gmail.com.br");
+		Fornecedor forn4 = new Fornecedor(null, "Católica", "51772565000128", "fornecedor04@gmail.com.br");
+		Fornecedor forn5 = new Fornecedor(null, "Novatec", "80915019000105", "fornecedor05@gmail.com.br");
+		Fornecedor forn6 = new Fornecedor(null, "Campus", "56456032000150", "fornecedor06@gmail.com.br");
+		Fornecedor forn7 = new Fornecedor(null, "Primeira Pessoa", "32366937000171", "fornecedor07@gmail.com.br");
+		Fornecedor forn8 = new Fornecedor(null, "Harvard", "83049934000145", "fornecedor08@gmail.com.br");
+		Fornecedor forn9 = new Fornecedor(null, "Editora USP", "17444064000184", "fornecedor09@gmail.com.br");
+		Fornecedor forn10 = new Fornecedor(null, "Viseu", "63747687000143", "fornecedor10@gmail.com.br");
+		fornecedorRepository.saveAll(Arrays.asList(forn1, forn2, forn3, forn4, forn5, forn6, forn7, forn8, forn9, forn10));
 		
 	}
 	
